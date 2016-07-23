536956704b1fcb5a92991574852e6c8ba7636430
Igor Machado Coelho <igor.machado@gmail.com>
2016-07-23 03:26:33 +0000

    created language model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160723031412) do
+ActiveRecord::Schema.define(version: 20160723032503) do
 
   create_table "competitions", force: :cascade do |t|
     t.string   "title"
@@ -25,6 +25,12 @@ ActiveRecord::Schema.define(version: 20160723031412) do
     t.datetime "end_time"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
+  end
+
+  create_table "languages", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "problems", force: :cascade do |t|
